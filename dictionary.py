1df0af5285a3f3263cf6c51a05ed97b274e68c38
unknown <you@example.com>
2023-10-09 18:22:28 +0600

    dictionary method

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,18 @@
 dict1 ={"name":"anisur rahman","roll":16}
 
-print(dict1["name"])
+# print(dict1["name"])
 
-print(dict1["roll"])
-print(dict1.keys())
-print(dict1.values())
-print(dict1.items())
+# print(dict1["roll"])
+# print(dict1.keys())
+# print(dict1.values())
+# print(dict1.items())
 
-x = dict1.get("name")
-print(x)
-dict1.pop("roll")
-print(dict1)
-dict1.popitem()
-print(dict1)
+# x = dict1.get("name")
+# print(x)
+# dict1.pop("roll")
+# print(dict1)
+# dict1.popitem()
+# print(dict1)
 
-dict1.update({"name":"rahad","roll":15})
+dict1.update({"rahad":15})
 print(dict1)
