071219994147a001458ebb5dee6b41776c1facb1
unknown <you@example.com>
2023-11-04 10:54:04 +0600

    2ddict

---
+++ nested_dict.py
@@ -0,0 +1,19 @@
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(len(myfamily))
+for i,j in myfamily.items():
+    print(i,j)
+    print(j["name"])
+    print(j["year"])
